70502d60fc596dda369458ad916af56c19a13b70
Moksh Chadha <mokshchadha@Mokshs-MacBook-Pro.local>
2025-11-02 18:48:35 +0530

    route place order and fetch the books implemented

--- go.mod
+++ go.mod
@@ -2,10 +2,14 @@ module exchange
 
 go 1.25.1
 
-require github.com/valyala/fasthttp v1.68.0
+require (
+	github.com/fasthttp/router v1.5.4
+	github.com/valyala/fasthttp v1.68.0
+)
 
 require (
 	github.com/andybalholm/brotli v1.2.0 // indirect
 	github.com/klauspost/compress v1.18.1 // indirect
+	github.com/savsgio/gotils v0.0.0-20240704082632-aef3928b8a38 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,19 +1,34 @@
 package main
 
 import (
+	"encoding/json"
 	"exchange/orderbook"
 
+	"github.com/fasthttp/router"
 	"github.com/valyala/fasthttp"
 )
 
 func main() {
-	fasthttp.ListenAndServe(":3000", handlePlaceOrder)
+	ex := NewExchange()
+	r := router.New()
+	r.POST("/order", ex.handlePlaceOrder)
+	r.GET("/books/{marketId}", ex.handleGetBook)
+	fasthttp.ListenAndServe(":3000", r.Handler)
 }
+
+const invaliErrorStr = `{"error": "Invalid Error"}`
+
+type OrderType string
+
+const (
+	MarketOrder OrderType = "MARKET"
+	LimiteOrder OrderType = "LIMIT"
+)
 
 type Market string
 
 const (
-	MaketETH Market = "ETH"
+	MarketETH Market = "ETH"
 )
 
 type Exchange struct {
@@ -21,13 +36,106 @@ type Exchange struct {
 }
 
 func NewExchange() *Exchange {
+	orderbooks := make(map[Market]*orderbook.OrderBook)
+	orderbooks[MarketETH] = orderbook.NewOrderBook()
+
 	return &Exchange{
-		orderbooks: make(map[Market]*orderbook.OrderBook),
+		orderbooks: orderbooks,
 	}
 }
 
-func handlePlaceOrder(ctx *fasthttp.RequestCtx) {
+type PlaceOrderRequest struct {
+	Type   OrderType // limit or market
+	Bid    bool
+	Size   float64
+	Price  float64
+	Market Market
+}
+
+func (ex *Exchange) handlePlaceOrder(ctx *fasthttp.RequestCtx) {
 	ctx.SetContentType("application/json")
-	ctx.SetStatusCode(200)
-	ctx.SetBodyString(`"hello world"`)
+	body := ctx.PostBody()
+	var placeOrderReq PlaceOrderRequest
+	err := json.Unmarshal(body, &placeOrderReq)
+
+	if err != nil {
+		ctx.SetStatusCode(fasthttp.StatusBadRequest)
+		ctx.SetBodyString(invaliErrorStr)
+		return
+	}
+	market := Market(placeOrderReq.Market)
+
+	ob := ex.orderbooks[market]
+	order := orderbook.NewOrder(placeOrderReq.Bid, placeOrderReq.Size)
+	ob.PlaceLimitOrder(placeOrderReq.Price, order)
+
+	ctx.SetStatusCode(fasthttp.StatusOK)
+	ctx.SetBodyString(`{"message": "Order placed successfully"}`)
+}
+
+type Order struct {
+	Price     float64
+	Size      float64
+	Bid       bool
+	Timestamp int64
+}
+
+type OrderBookData struct {
+	Asks []*Order
+	Bids []*Order
+}
+
+func (ex *Exchange) handleGetBook(ctx *fasthttp.RequestCtx) {
+	marketId := Market(ctx.UserValue("marketId").(string))
+
+	ob, ok := ex.orderbooks[marketId]
+
+	if !ok {
+		ctx.SetStatusCode(fasthttp.StatusBadRequest)
+		ctx.SetBodyString(invaliErrorStr)
+		return
+	}
+
+	orderbookData := OrderBookData{
+		Asks: []*Order{},
+		Bids: []*Order{},
+	}
+
+	for _, limit := range ob.Asks() {
+		for _, order := range limit.Orders {
+			o := Order{
+				Price:     limit.Price,
+				Size:      order.Size,
+				Bid:       order.Bid,
+				Timestamp: order.Timestamp,
+			}
+
+			orderbookData.Asks = append(orderbookData.Asks, &o)
+		}
+
+	}
+
+	for _, limit := range ob.Bids() {
+		for _, order := range limit.Orders {
+			o := Order{
+				Price:     limit.Price,
+				Size:      order.Size,
+				Bid:       order.Bid,
+				Timestamp: order.Timestamp,
+			}
+
+			orderbookData.Bids = append(orderbookData.Bids, &o)
+		}
+
+	}
+
+	// instead send the orderbook data
+	jsonData, err := json.Marshal(orderbookData)
+	if err != nil {
+		ctx.SetStatusCode(fasthttp.StatusBadRequest)
+		ctx.SetBodyString(invaliErrorStr)
+		return
+	}
+	ctx.SetStatusCode(fasthttp.StatusOK)
+	ctx.SetBody(jsonData)
 }
