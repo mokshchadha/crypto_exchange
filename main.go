a1343bdbfcebc704cb8211ad5b30a77a30f977bf
Moksh Chadha <mokshchadha@Mokshs-MacBook-Pro.local>
2025-11-24 20:58:16 +0530

    first transaction rough working

--- main.go
+++ main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"context"
+	"crypto/ecdsa"
 	"encoding/json"
 	"exchange/orderbook"
 	"fmt"
+	"log"
+	"math/big"
 	"strconv"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
 
 	"github.com/fasthttp/router"
 	"github.com/valyala/fasthttp"
@@ -16,6 +25,67 @@ func main() {
 	r.POST("/order", ex.handlePlaceOrder)
 	r.POST("/cancel/{orderId}", ex.handleCancelOrder)
 	r.GET("/books/{marketId}", ex.handleGetBook)
+	ganacheServer := "http://127.0.0.1:8545"
+	client, err := ethclient.Dial(ganacheServer)
+
+	if err != nil {
+		log.Fatal("Some error while connecting with ganache")
+	}
+
+	ctx := context.Background()
+	firstAddress := "0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1" // update this as per the ganache server
+	address := common.HexToAddress(firstAddress)
+
+	privateKey, err := crypto.HexToECDSA("4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d") // remove the 0x from the ganche pvt key
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+
+	if !ok {
+		log.Fatal("error casting public key to ECDSA")
+	}
+
+	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	value := big.NewInt(1000000000000000000) // in wei (1 eth)
+	gasLimit := uint64(21000)                //
+
+	gasPrice, err := client.SuggestGasPrice(context.Background())
+
+	toAddress := common.HexToAddress("0x1dF62f291b2E969fB0849d99D9Ce41e2F137006e")
+
+	tx := types.NewTransaction(nonce, toAddress, value, gasLimit, gasPrice, nil)
+	fmt.Println(tx)
+
+	// chainID, err := client.NetworkID(context.Background())
+	chainID := big.NewInt(1337)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	signedTx, err := types.SignTx(tx, types.NewEIP155Signer(chainID), privateKey)
+
+	if err != nil {
+		log.Fatal(err)
+
+	}
+	err = client.SendTransaction(context.Background(), signedTx)
+
+	balance, err := client.BalanceAt(ctx, address, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(balance)
+
 	fasthttp.ListenAndServe(":3000", r.Handler)
 }
 
